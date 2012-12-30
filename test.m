1e1c04780ba3a8867f7f3eacdb6f1fc7ed70a4a0
Mai <lmaib_3@yahoo.de>
2012-12-30 17:54:59 +0100

    huhu

---
+++ test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  Fat Ninja
+//
+//  Created by Linda Mai Bui on 12/30/12.
+//  Copyright (c) 2012 Florian Weiß. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
