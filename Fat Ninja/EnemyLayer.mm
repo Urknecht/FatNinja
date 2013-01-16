3d0475316e1c8fc646d7fe9b0c83100124a654bf
Mai <lmaib_3@yahoo.de>
2013-01-16 11:40:06 +0100

    sushi kleiner gemacht, ninja bisschen weiter vor

--- Fat Ninja/EnemyLayer.mm
+++ Fat Ninja/EnemyLayer.mm
@@ -32,7 +32,7 @@ int tag; //vorlaeufige variable zum auswaehlen welcher gegner auftaucht
     if (self != nil) {
         tag=0;
         nextStage=false;
-        
+        enemyCounter=0;
         praesentationCounter = 0;
 
         geschwindigkeitEnemy=5.0;
@@ -74,6 +74,7 @@ int tag; //vorlaeufige variable zum auswaehlen welcher gegner auftaucht
         case 1:
             tag++;
             enemy=[[Sushi alloc] init];
+            enemy.scale =0.7;
             break;
         case 2:
             tag++;
--- Fat Ninja/GameLayer.mm
+++ Fat Ninja/GameLayer.mm
@@ -87,6 +87,7 @@ UITouch *lastTouch;
         //Sushi anzeige
         sushiCounter=0;
         CCSprite *sushiImage= [CCSprite spriteWithFile:@"sushi.png"];
+        sushiImage.scale =0.5;
         [self addChild:sushiImage];
         sushiImage.position= ccp(winSize.width-80, winSize.height-50);
         
